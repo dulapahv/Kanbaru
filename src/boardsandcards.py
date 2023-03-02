fe821257d7569d1159d179d61e8afefd6d3a032f
Anucha Chewachanon <68524576+SpiralNuggets@users.noreply.github.com>
2023-03-02 14:10:31 +0700

    Added KanbanLists
    Please note that this is not production ready

--- src/boardsandcards.py
+++ src/boardsandcards.py
@@ -9,45 +9,6 @@ class Color(Enum):
     LAVENDER = "#c577dc"
     TEAL = "#5eb5c1"
 
-
-class Board:
-    def __init__(self, id: int = random.randint(0, 1000000), color: Color = Color.LIGHTBLUE, title: str = "New Board"):
-        self.__title = title
-        self.__id = id
-        self.__color = color
-        self.__member = []
-
-    def get_title(self):
-        return self.__title
-
-    def get_id(self):
-        return self.__id
-    
-    def get_color(self):
-        return self.__color
-    
-    def get_member(self):
-        return self.__member
-    
-    def set_title(self, title):
-        self.__title = title
-    
-    def set_color(self, color):
-        if color in Color:
-            self.__color = color
-        else:
-            raise ValueError("Invalid color. We only support the following colors: " + str(Color))
-
-    def add_member(self, member):
-        self.__member.append(member)
-
-    def remove_member(self, member):
-        self.__member.remove(member)
-
-    def __str__(self):
-        return str(self.__id)
-
-
 #access card through board?
 #TODO: figure out the datetime system for Qt
 class Card:
@@ -93,4 +54,82 @@ class Card:
     def set_time(self, time):
         self.__time = time
 
+class KanbanList:
+    def __init__(self, title: str = "New List", color: Color = Color.LIGHTBLUE):
+        self.__title = title
+        self.__color = color
+        self.__cards = []
+
+    def get_title(self):
+        return self.__title
+
+    def get_color(self):
+        return self.__color
     
+    def get_cards(self):
+        return self.__cards
+    
+    def set_title(self, title):
+        self.__title = title
+    
+    def set_color(self, color):
+        if color in Color:
+            self.__color = color
+        else:
+            raise ValueError("Invalid color. We only support the following colors: " + str(Color))
+    
+    def add_card(self, card: Card):
+        if card is not Card:
+            raise ValueError("Card is not a Card.")
+        if card in self.__cards:
+            raise ValueError("Card already exists in list.")
+        self.__cards.append(card)
+    
+    def remove_card(self, card: Card):
+        if card not in self.__cards:
+            raise ValueError("Card does not exist in list.")
+        self.__cards.remove(card)
+
+    def __str__(self):
+        return self.__title
+    
+class Board:
+    def __init__(self, id: int = random.randint(0, 1000000), color: Color = Color.LIGHTBLUE, title: str = "New Board"):
+        self.__title = title
+        self.__id = id
+        self.__color = color
+        self.__lists = []
+
+    def get_title(self):
+        return self.__title
+
+    def get_id(self):
+        return self.__id
+    
+    def get_color(self):
+        return self.__color
+    
+    def get_lists(self):
+        return self.__lists
+    
+    def set_title(self, title):
+        self.__title = title
+    
+    def set_color(self, color):
+        if color in Color:
+            self.__color = color
+        else:
+            raise ValueError("Invalid color. We only support the following colors: " + str(Color))
+
+    def add_list(self, member: KanbanList):
+        if member in self.__lists:
+            raise ValueError("List already exists in board.")
+        self.__lists.append(member)
+
+    def remove_list(self, member: KanbanList):
+        if member not in self.__lists:
+            raise ValueError("List does not exist in board.")
+        self.__lists.remove(member)
+
+    def __str__(self):
+        return str(self.__id)
