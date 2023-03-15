c46bdd0c025e58bea00b01daa968180f2ef0cb0f
Dulapah Vibulsanti <dulapah6@gmail.com>
2023-03-15 09:33:27 +0700

    Test custom font

--- src/main.py
+++ src/main.py
@@ -9,39 +9,23 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
-        fontDatabase = QFontDatabase()
-        fontDatabase.addApplicationFont("./resources/font/TorusPro-Bold.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-BoldItalic.otf")
-        fontDatabase.addApplicationFont("./resources/font/TorusPro-Italic.otf")
-        fontDatabase.addApplicationFont("./resources/font/TorusPro-Light.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-LightItalic.otf")
-        fontDatabase.addApplicationFont("./resources/font/TorusPro-Medium.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-MediumItalic.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-Regular.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-SemiBold.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-SemiBoldItalic.otf")
-        fontDatabase.addApplicationFont("./resources/font/TorusPro-Thin.otf")
-        fontDatabase.addApplicationFont(
-            "./resources/font/TorusPro-ThinItalic.otf")
+        fontDatabase = QFontDatabase.addApplicationFont(
+            "src\\resources\\font\\TorusPro-Regular.ttf")
+        if fontDatabase < 0:
+            sys.exit("Font not found")
+        fontFamilies = QFontDatabase.applicationFontFamilies(fontDatabase)
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         for obj in self.findChildren(QObject):
             if hasattr(obj, "setFont"):
-                print(obj)
-                obj.setFont(QFont("Torus Pro", 12))
-        self.ui.label_list_1.setFont(QFont("Torus Pro", 12, QFont.Bold))
-        self.ui.label_list_2.setFont(QFont("Torus Pro", 12, QFont.Bold))
-        self.ui.label_list_3.setFont(QFont("Torus Pro", 12, QFont.Bold))
-        self.ui.label_logo.setFont(QFont("Torus Pro", 36))
-        self.ui.label_board.setFont(QFont("Torus Pro", 28))
+                obj.setFont(QFont(fontFamilies[0], 12))
+        self.ui.label_list_1.setFont(QFont(fontFamilies[0], 12, QFont.Bold))
+        self.ui.label_list_2.setFont(QFont(fontFamilies[0], 12, QFont.Bold))
+        self.ui.label_list_3.setFont(QFont(fontFamilies[0], 12, QFont.Bold))
+        self.ui.label_logo.setFont(QFont(fontFamilies[0], 36))
+        self.ui.label_board.setFont(QFont(fontFamilies[0], 28))
 
 
 def main():
