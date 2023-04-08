3f9e3aceb29391a8d9ba830851db2034ab028575
Dulapah Vibulsanti <dulapah6@gmail.com>
2023-04-08 13:13:33 +0700

    Update app_settings_ui.py

--- src/ui/app_settings_ui.py
+++ src/ui/app_settings_ui.py
@@ -303,7 +303,7 @@ class Ui_SettingsWindow(object):
         self.verticalLayout_3.addWidget(self.widget)
 
         SettingsWindow.setCentralWidget(self.centralwidget)
-        QWidget.setTabOrder(self.widget1, self.listWidget_manage_board)
+        QWidget.setTabOrder(self.widget, self.listWidget_manage_board)
         QWidget.setTabOrder(self.listWidget_manage_board, self.btn_delete)
         QWidget.setTabOrder(self.btn_delete, self.btn_rename)
         QWidget.setTabOrder(self.btn_rename, self.btn_logout)
